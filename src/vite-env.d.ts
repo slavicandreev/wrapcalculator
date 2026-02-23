e7bae3ef32855fd0efe8fda39d13ea7480d8cb08
Slavic <test@gmail.com>
2026-02-22 18:16:43 -0600

    chore: add TypeScript declaration for *.css?inline imports

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+// Teach TypeScript that *.css?inline imports resolve to a string
+declare module '*.css?inline' {
+  const content: string;
+  export default content;
+}
